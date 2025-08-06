8081a16a6c0b252d4a7a6a440661265c17a00dac
Charbel Haber <charbelh@beytek.co>
2025-08-06 12:15:47 +0300

    fixing database connection

--- models/index.js
+++ models/index.js
@@ -1,8 +1,8 @@
 const { Sequelize, DataTypes } = require('sequelize');
 
-const sequelize = new Sequelize('pre_smartriselocal', 'root', 'Charbel_h123', {
-    host: 'localhost',
-    dialect: 'mysql',
+const sequelize = new Sequelize(process.env.DB_DATABASE, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_CONNECTION,
     logging: false,
 });
 
